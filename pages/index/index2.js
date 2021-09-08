d015c17abbcf64a6b3ee4caf7552852b0a75126f
yccx <wuweihong08@gmail.com>
2021-09-08 23:26:21 +0800

    首页登入完成版

--- pages/index/index2.js
+++ pages/index/index2.js
@@ -1,87 +1,66 @@
-// index.js
+// pages/index/index2.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo:{},
-    hasUserInfo:false,
-    canIUseGetUserProfile:false,
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {
-    if(wx.getUserProfile){
-      this.setData({canIUseGetUserProfile:true
-      })
-    } 
-  },
+  onLoad: function (options) {
 
-  getUserProfile(e){
-    wx.getUserProfile({
-      desc: '用于完善用户资料',
-      success:(res)=>{
-        this.setData({
-          userInfo:res.userInfo,
-          hasUserInfo:true,
-          
-        })
-      }
-    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
-
 })
-
-
